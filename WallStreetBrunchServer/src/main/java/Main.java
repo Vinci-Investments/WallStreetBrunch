0c989062fc44b48cb38d19295f84bbeeca49c15c
victorqrt <vquerette@gmail.com>
2018-11-23 10:27:17 +0100

    Maven

--- WallStreetBrunchServer/src/main/java/Main.java
+++ WallStreetBrunchServer/src/main/java/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application
 {
     @Override public void start(Stage primaryStage) throws Exception
     {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample.fxml"));
         Parent root = loader.load();
         primaryStage.setTitle("VI WSBrunch Server");
         primaryStage.setScene(new Scene(root));
